4a6de923795c23a0ad026123f4d79b8e8ff03009
wasim ullah 209 <wasimcs88@gmail.com>
2025-05-11 00:40:01 +0500

    adding the write command for testing

--- fileread.py
+++ fileread.py
@@ -1,9 +1,9 @@
 # file i/o
-f = open ("pk.txt", "r")
-data = f.read()
-print(data)
-print(type(data))
-f.close()
+# f = open ("pk.txt", "r")
+# data = f.read()
+# print(data)
+# print(type(data))
+# f.close()
 
 f = open ("pk.txt", "a")
 f.write("testing that how write work in file i.o")
