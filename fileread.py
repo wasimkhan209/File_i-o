e5b072dc877da25decf3f2cfa57f9b01cc842851
wasim ullah 209 <wasimcs88@gmail.com>
2025-05-11 00:49:23 +0500

    Test again and again

--- fileread.py
+++ fileread.py
@@ -5,6 +5,10 @@
 # print(type(data))
 # f.close()
 
-f = open ("pk.txt", "a")
-f.write("Testing that how write work in file i.o")
+# f = open ("pk.txt", "a")
+# f.write("Please Test that how write work in file i.o")
+# f.close()
+
+f = open("pk.txt", "a")
+f.write("Testing again and again coz to clear the concept")
 f.close()
