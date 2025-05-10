0ccbd2eab086c52a9a870460ea8f1b4c79032241
wasim ullah 209 <wasimcs88@gmail.com>
2025-05-11 01:37:46 +0500

    adding the new code of with option

--- fileread.py
+++ fileread.py
@@ -12,3 +12,6 @@
 f = open("pk.txt", "a")
 f.write("Testing again and again coz to clear the concept")
 f.close()
+
+with open("newpractice.txt" , "a+") as f:
+    f.write("Hi, everyone \n" + "i'm learning python \n" + "so, i'm very excited for learning python \n" + "Thanks to all of you")
